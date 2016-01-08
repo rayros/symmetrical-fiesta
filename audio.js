d310f14e27b36290336451f7ba6432cb9e891af9
Paweł Łaski <zniwus@gmail.com>
2016-01-08 16:11:09 +0100

    add getUserMedia polyfill

--- audio.js
+++ audio.js
@@ -2,7 +2,7 @@ var audioCtx = new AudioContext(),
     analyser = audioCtx.createAnalyser();
     draw = new Draw('audioAnalyzer', analyser, 'rgb(200, 0, 200)');
 draw.start();
-navigator.webkitGetUserMedia({audio: true},
+navigator.getUserMedia({audio: true},
   function(stream) {
     var source = audioCtx.createMediaStreamSource(stream);
     source.connect(analyser);
